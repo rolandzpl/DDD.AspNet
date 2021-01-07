389f38fb264a1cbca3250a300180fe2a534b1938
Roland <rolandz@poczta.fm>
2021-01-07 15:57:23 +0100

    Implemented ApplicationBuilderExtensions

--- src/DDD.AspNet/ApplicationBuilderExtensions.cs
+++ src/DDD.AspNet/ApplicationBuilderExtensions.cs
@@ -1,11 +1,43 @@
+using DDD.Domain;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DDD
 {
     public static class ApplicationBuilderExtensions
     {
+        public static IApplicationBuilder PublishEventsToMessageBus(this IApplicationBuilder app)
+        {
+            var eventStore = app.ApplicationServices.GetRequiredService<IEventStore>();
+            var bus = app.ApplicationServices.GetRequiredService<IMessageBus>();
+            eventStore.NewEvents += (s, e) =>
+            {
+                foreach (var _ in e.NewEvents)
+                {
+                    try
+                    {
+                        bus.Publish(_);
+                    }
+                    catch { }
+                }
+            };
+            return app;
+        }
+
         public static IApplicationBuilder ReplayAllEvents(this IApplicationBuilder app)
         {
+            var eventStore = app.ApplicationServices.GetRequiredService<IEventStore>();
+            var bus = app.ApplicationServices.GetRequiredService<IMessageBus>();
+            eventStore.GetAllEvents();
+            foreach (var eventId in eventStore.GetAllEvents())
+            {
+                try
+                {
+                    var e = eventStore.GetEventsById(eventId);
+                    bus.Publish(e);
+                }
+                catch { }
+            }
             return app;
         }
     }
